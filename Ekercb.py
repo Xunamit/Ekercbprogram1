d0f1bfb67c899b4999237825b45e0046c2ab5349
Xunamit <68418985+Xunamit@users.noreply.github.com>
2025-02-16 10:47:56 +0100

    Update Ekercb.py

--- Ekercb.py
+++ Ekercb.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import difflib  # Rugalmas kereséshez
 
 # WordPress API URL (több cikk lekérése)
-WORDPRESS_API_URL = "https://www.egyenisegepites.hu/wp-json/wp/v2/posts?per_page=500"
+WORDPRESS_API_URL = "https://www.egyenisegepites.hu/wp-json/wp/v2/posts?per_page=200"
 
 # HTML eltávolítása
 def clean_html(text):
